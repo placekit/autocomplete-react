643afc9fd461d04d83b1d750e23e3dde275160f3
noclat <noclat@gmail.com>
2023-02-01 22:13:01 +0100

    doc:add tzc example

---
+++ examples/autocomplete-react-tzc/src/index.jsx
@@ -0,0 +1,23 @@
+import { createRoot } from 'react-dom/client';
+
+import './global.css';
+import '@placekit/autocomplete-js/dist/placekit-autocomplete.css';
+
+import { PlaceKit } from '@placekit/autocomplete-react';
+import tzc from '@placekit/tzc';
+
+const App = () => (
+  <div className="flex justify-center">
+    <PlaceKit
+      apiKey={process.env.PLACEKIT_API_KEY}
+      options={{
+        countries: tzc(),
+      }}
+      className="w-96"
+    />
+  </div>
+);
+
+const app = document.getElementById('app');
+const root = createRoot(app);
+root.render(<App />);
