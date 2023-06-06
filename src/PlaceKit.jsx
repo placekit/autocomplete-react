8ec311ff3dcdcc820821ff5d72154cb4542d7dcc
Nicolas Torres <1834312+noclat@users.noreply.github.com>
2023-06-06 13:42:34 +0200

    fix:object literals re-render (#19)
    * document object literals re-render and filter native input props
    
    * remove map-props
    
    * use useStableValue (thanks Francois Chalifour) in usePlaceKit
    
    * fix lint

--- src/PlaceKit.jsx
+++ src/PlaceKit.jsx
@@ -1,52 +1,22 @@
 import PropTypes from 'prop-types';
-import React, { forwardRef, useEffect, useMemo } from 'react';
+import React, { forwardRef, memo, useEffect } from 'react';
 
 import { usePlaceKit } from './usePlaceKit';
 
-const PlaceKit = forwardRef(({
-  apiKey,
-  useGeolocation,
-  className,
-  options,
-  onOpen,
-  onClose,
-  onResults,
-  onPick,
-  onError,
-  onGeolocation,
-  onEmpty,
-  onFreeForm,
-  ...inputProps
-}, ref) => {
-  const opts = useMemo(
-    () => {
-      const output = {
-        handlers: {},
-        ...options
-      };
-      output.handlers.onOpen = onOpen;
-      output.handlers.onClose = onClose;
-      output.handlers.onResults = onResults;
-      output.handlers.onPick = onPick;
-      output.handlers.onError = onError;
-      output.handlers.onGeolocation = onGeolocation;
-      output.handlers.onEmpty = onEmpty;
-      output.handlers.onFreeForm = onFreeForm;
-      return output;
+const PlaceKit = memo(forwardRef((props, ref) => {
+  const { target, client, state } = usePlaceKit(props.apiKey, {
+    ...props.options,
+    handlers: {
+      onOpen: props.onOpen,
+      onClose: props.onClose,
+      onResults: props.onResults,
+      onPick: props.onPick,
+      onError: props.onError,
+      onGeolocation: props.onGeolocation,
+      onEmpty: props.onEmpty,
+      onFreeForm: props.onFreeForm,
     },
-    [
-      options,
-      onOpen,
-      onClose,
-      onResults,
-      onPick,
-      onError,
-      onGeolocation,
-      onFreeForm,
-    ]
-  );
-  
-  const { target, client, state } = usePlaceKit(apiKey, opts);
+  });
 
   useEffect(
     () => {
@@ -65,10 +35,10 @@ const PlaceKit = forwardRef(({
     <div
       className={[
         'pka-input',
-        className
+        props.className
       ].filter((c) => c).join(' ')}
     >
-      {!!useGeolocation && (
+      {!!props.useGeolocation && (
         <button
           type="button"
           className={[
@@ -79,7 +49,7 @@ const PlaceKit = forwardRef(({
           role="switch"
           aria-checked={state.hasGeolocation}
           onClick={client?.requestGeolocation}
-          disabled={inputProps.disabled}
+          disabled={props.disabled}
         >
           <span className="pka-sr-only">Activate geolocation</span>
         </button>
@@ -90,29 +60,37 @@ const PlaceKit = forwardRef(({
         title="Clear value"
         aria-hidden={state.isEmpty}
         onClick={client?.clear}
-        disabled={inputProps.disabled}
+        disabled={props.disabled}
       >
         <span className="pka-sr-only">Clear value</span>
       </button>
       <input
-        placeholder="Search places..."
-        {...inputProps}
         ref={target}
+        id={props.id}
+        name={props.name}
+        placeholder={props.placeholder}
+        disabled={props.disabled}
+        required={props.required}
+        autoFocus={props.autoFocus}
         type="search"
       />
     </div>
   );
-});
+}));
 
 PlaceKit.defaultProps = {
   useGeolocation: true,
   options: {},
+  placeholder: "Search places...",
 };
 
 PlaceKit.propTypes = {
-  apiKey: PropTypes.string.isRequired,
+  // component options
   useGeolocation: PropTypes.bool,
   className: PropTypes.string,
+
+  // PlaceKit Autocomplete JS options
+  apiKey: PropTypes.string.isRequired,
   options: PropTypes.shape({
     offset: PropTypes.number,
     template: PropTypes.func,
@@ -128,6 +106,8 @@ PlaceKit.propTypes = {
     countries: PropTypes.arrayOf(PropTypes.string),
     coordinates: PropTypes.string,
   }),
+
+  // event handlers
   onOpen: PropTypes.func,
   onClose: PropTypes.func,
   onResults: PropTypes.func,
@@ -136,6 +116,14 @@ PlaceKit.propTypes = {
   onEmpty: PropTypes.func,
   onFreeForm: PropTypes.func,
   onGeolocation: PropTypes.func,
+
+  // native HTML input props
+  id: PropTypes.string,
+  name: PropTypes.string,
+  placeholder: PropTypes.string,
+  disabled: PropTypes.bool,
+  required: PropTypes.bool,
+  autoFocus: PropTypes.bool,
 };
 
 export default PlaceKit;
---
+++ src/useStableValue.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+// dequal borrowed from https://github.com/lukeed/dequal/blob/master/src/lite.js
+const has = Object.prototype.hasOwnProperty;
+
+const dequal = (foo, bar) => {
+  let ctor; let len;
+  if (foo === bar) return true;
+
+  if (foo && bar && (ctor=foo.constructor) === bar.constructor) {
+    if (ctor === Date) return foo.getTime() === bar.getTime();
+    if (ctor === RegExp) return foo.toString() === bar.toString();
+
+    if (ctor === Array) {
+      if ((len=foo.length) === bar.length) {
+        while (len-- && dequal(foo[len], bar[len]));
+      }
+      return len === -1;
+    }
+
+    if (!ctor || typeof foo === 'object') {
+      len = 0;
+      // eslint-disable-next-line guard-for-in
+      for (ctor in foo) {
+        if (has.call(foo, ctor) && ++len && !has.call(bar, ctor)) return false;
+        if (!(ctor in bar) || !dequal(foo[ctor], bar[ctor])) return false;
+      }
+      return Object.keys(bar).length === len;
+    }
+  }
+
+  return foo !== foo && bar !== bar;
+};
+
+// prevent re-renders when options prop is an object literal
+export const useStableValue = (value) => {
+  const [stableValue, setStableValue] = useState(() => value);
+  if (!dequal(stableValue, value)) {
+    setStableValue(value);
+  }
+  return stableValue;
+};
